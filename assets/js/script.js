38088d252cacbf0d55e9e36be98fc85b39073c0d
Dolores Moriconi <dolomoriconi@gmail.com>
2024-02-27 23:55:05 -0300

    correccion de la 1er formula en js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-////accion para aparecer y desaparecer el contenido de todas las tarjetas, cuando el usuario haga doble clic en caalquiera de los tres bloques de los iconos.
+////accion para aparecer y desaparecer el contenido de todas llas filas, cuando el usuario haga doble clic en cualquiera de los tres bloques de los iconos.
 document.addEventListener("DOMContentLoaded", function() {
     const iconSection = document.getElementById("caja-de-texto");
 
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-//accion para aparecer y desaparecer el contenido de todas las tarjetas, cuando el usuario haga clic en caalquiera de las cuatro imagenes.
+//accion para aparecer y desaparecer el contenido de todas las tarjetas, cuando el usuario haga clic en cualquiera de las cuatro imagenes.
 document.addEventListener("DOMContentLoaded", function() {
     let cards = document.querySelectorAll(".card");
     let bodies = document.querySelectorAll(".card-body");
@@ -30,18 +30,8 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-//accion para que con un clic en enviar se notifique un agradecimiento por contactarse y confirmando quue se le respondera.
+//accion para que con un clic en enviar se notifique un agradecimiento por contactarse y confirmando que se le respondera.
 $("#envio-contacto").on('click', function () {
     alert("Gracias por contactarte con Viajes Chile, te responderemos a la brevedad.");
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const iconSection = document.getElementById("caja-de-texto");
-
-    iconSection.addEventListener("dblclick", function() {
-        const textElements = iconSection.querySelectorAll("p");
-        textElements.forEach(element => {
-            element.style.display = element.style.display === "none" ? "" : "none";
-        });
-    });
-});
